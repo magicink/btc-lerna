52944104a1ec437b6fddf20e34ce563a3fd0c150
Brandon Tom <magicink@gmail.com>
2021-11-26 07:31:38 -0800

    created generator-brandontom-prettier

---
+++ packages/generator-brandontom-prettier/README.md
@@ -0,0 +1,10 @@
+# `generator-brandontom-prettier`
+
+> Generates Prettier files and scripts.
+
+## Usage
+
+```
+npm i -g generator-brandontom-prettier
+yo brandontom-prettier
+```
---
+++ packages/generator-brandontom-prettier/app/index.js
@@ -0,0 +1,17 @@
+const Generator = require('yeoman-generator')
+module.exports = class extends Generator {
+  async writing () {
+    this.fs.copyTpl(this.templatePath('prettierignore'), this.destinationPath('.prettierignore'))
+    const packageSettings = {
+      prettier: '@brandontom/prettier',
+      optionalDependencies: {
+        '@brandontom/prettier': require('@brandontom/prettier/package.json').version
+      },
+      scripts: {
+        format:
+          'npx sort-package-json && prettier --write "**/*.{js,jsx,json,md,html,scss,css}"'
+      }
+    }
+    this.fs.extendJSON(this.destinationPath('package.json'), packageSettings)
+  }
+}
