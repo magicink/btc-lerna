24e5551562cae7639fd506bff06e459d36cd26ef
Brandon Tom <magicink@gmail.com>
2021-12-16 13:30:33 -0800

    created distributable

--- packages/btc-babel-preset/src/index.js
+++ packages/btc-babel-preset/src/index.js
@@ -2,9 +2,29 @@ const presets = [
   [
     '@babel/preset-env',
     {
-      targets: {
-        node: 'current'
-      }
+      forceAllTransforms: true,
+      modules: false
     }
-  ]
+  ],
+  [
+    '@babel/preset-react',
+    {
+      development: process.env.NODE_ENV !== 'production'
+    }
+  ],
+  ['@babel/preset-flow']
 ]
+const env = {
+  production: {
+    plugins: [
+      ['babel-plugin-react-remove-properties', { properties: ['data-testid'] }]
+    ]
+  }
+}
+const preset = _ => {
+  return {
+    env,
+    presets
+  }
+}
+export default preset
