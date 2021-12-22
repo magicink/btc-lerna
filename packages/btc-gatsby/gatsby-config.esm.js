2e6f5ca9fa7e547b354a85bce8158dc1813ee1a3
Brandon Tom <magicink@gmail.com>
2021-12-22 10:18:22 -0800

    moved wordpress config data to ignored .env file

--- packages/btc-gatsby/gatsby-config.esm.js
+++ packages/btc-gatsby/gatsby-config.esm.js
@@ -1,4 +1,18 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 const config = {
-  plugins: []
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      options: {
+        url: process.env.WPGRAPHQL
+      },
+      resolve: 'gatsby-source-wordpress'
+    }
+  ]
 }
+
 export default config
