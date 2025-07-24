aa51a84be2ff126935bd77e77ac9d178aee71036
Brandon Tom <magicink@gmail.com>
2025-07-23 17:05:19 -0700

    chore: remove `yarn.lock` file

--- packages/btc-jest/jest.config.ts
+++ packages/btc-jest/jest.config.ts
@@ -2,9 +2,7 @@ import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/file-mock.js'
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy'
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   testEnvironment: 'jsdom',
--- packages/btc-jest/src/test.ts
+++ packages/btc-jest/src/test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks'
+import { act, renderHook } from '@testing-library/react'
 import jestConfig from '../jest.config'
 import React from 'react'
 
